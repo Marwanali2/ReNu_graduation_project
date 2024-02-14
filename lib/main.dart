267e1114f683ed249163b3a8555cd13483888b84
Anan Hamdy <nonahamdy205@gmail.com>
2024-02-15 01:39:14 +0200

    remove debugShowCheckedModeBanner

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SplashView(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:  SplashView(),
     );
   }
 }
