98cf6a05885d2cd2540d8b7ba27c7737c5de922d
Anan Hamdy <nonahamdy205@gmail.com>
2024-02-15 01:31:05 +0200

    add logo and create splash view  body animation

---
+++ lib/features/splash/presentaion/view/widgets/splash_view_body.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:graduation_project/features/splash/presentaion/view/widgets/sliding_image.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+
+    initSliding();
+    slidingAnimation.addListener(() {});
+    //navigateToHome();
+  }
+
+  // void navigateToHome() {
+  //   Future.delayed(const Duration(seconds: 2), () {
+  //     Get.to(const HomeView(),transition: Transition.fade,duration: ktransationDuration);
+  //    GoRouter.of(context).push(AppRouter.kAuth);
+  //   });
+  // }
+
+  void initSliding() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    super.initState();
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const SizedBox(
+          height: 5,
+        ),
+        SlidingImage(slidingAnimation: slidingAnimation)
+      ],
+    );
+  }
+}
+
+
+
+
