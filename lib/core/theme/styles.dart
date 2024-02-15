c358a5a852d7b4c1bf476709e3a8f169a430cf08
Anan Hamdy <nonahamdy205@gmail.com>
2024-02-15 15:27:59 +0200

    add roboto font to assests

---
+++ lib/core/theme/styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+abstract class Styles{
+   static const textStyle32=TextStyle(
+fontSize: 32,
+fontWeight: FontWeight.w500,
+color:Color(0xff676767)
+ );
+
+ static const textStyle13=TextStyle(
+fontSize: 13,
+fontWeight: FontWeight.w400,
+color:Color(0xff676767)
+ );
+ static const textStyle14=TextStyle(
+fontSize: 14,
+fontWeight: FontWeight.w400,
+color:Color(0xff676767)
+ );
+}
--- lib/features/auth/presentation/views/widgets/log_or_sign_view_body.dart
+++ lib/features/auth/presentation/views/widgets/log_or_sign_view_body.dart
@@ -5,8 +5,32 @@ class LogOrSignViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 119),
+        child: Column(children: [
+          Image.asset('assests/images/logo.png'),
+          const SizedBox(height: 94,),
+          const Text(
+            'Sign Up',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w500,
+              color: Color(0xff676767)
+            ),
+          ),
+          const SizedBox(height: 13,),
+          const Text(
+            'it’s easier to sign up now',
+            style: TextStyle(
+              fontSize: 13,
+              fontWeight: FontWeight.w400,
+              color: Color(0xff676767)
+            ),
+          )
+
+        ]),
+      ),
     );
   }
 }
