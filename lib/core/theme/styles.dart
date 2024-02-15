073c5c17e497f2b76f63a8966bb5efa92772d9b3
Anan Hamdy <nonahamdy205@gmail.com>
2024-02-15 15:47:47 +0200

    add Inter font family to assests

--- lib/core/theme/styles.dart
+++ lib/core/theme/styles.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:graduation_project/core/helpers/constants.dart';
 abstract class Styles{
    static const textStyle32=TextStyle(
 fontSize: 32,
 fontWeight: FontWeight.w500,
-color:Color(0xff676767)
+color:Color(0xff676767),
+fontFamily: signFont
  );
 
  static const textStyle13=TextStyle(
 fontSize: 13,
 fontWeight: FontWeight.w400,
-color:Color(0xff676767)
+color:Color(0xff676767),
+fontFamily: signFont
+
  );
  static const textStyle14=TextStyle(
 fontSize: 14,
 fontWeight: FontWeight.w400,
-color:Color(0xff676767)
+color:Color(0xff676767),
+fontFamily: signFont
+
  );
+ static const textStyle30=TextStyle(
+fontSize: 30,
+fontWeight: FontWeight.w700,
+color:Color(0xff676767),
+fontFamily: signFont
+
+ );
+
 }
