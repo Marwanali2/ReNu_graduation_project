286444198bde394119bebeec095b1f7f7befe74e
Anan Hamdy <nonahamdy205@gmail.com>
2024-02-21 06:09:52 +0200

    add firebase and firebase auth

--- lib/features/auth/presentation/views/widgets/log_or_sign_view_body.dart
+++ lib/features/auth/presentation/views/widgets/log_or_sign_view_body.dart
@@ -7,9 +7,27 @@ import 'package:graduation_project/features/auth/presentation/views/widgets/cust
 import 'package:graduation_project/features/auth/presentation/views/widgets/custom_button_email.dart';
 import 'package:graduation_project/features/auth/presentation/views/widgets/custom_icon_button.dart';
 import 'package:graduation_project/features/auth/presentation/views/widgets/custom_text_widget.dart';
-
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 class LogOrSignViewBody extends StatelessWidget {
   const LogOrSignViewBody({super.key});
+
+Future<UserCredential> signInWithGoogle() async {
+  // Trigger the authentication flow
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+  // Obtain the auth details from the request
+  final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+  // Create a new credential
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth?.accessToken,
+    idToken: googleAuth?.idToken,
+  );
+
+  // Once signed in, return the UserCredential
+  return await FirebaseAuth.instance.signInWithCredential(credential);
+}
 
   @override
   Widget build(BuildContext context) {
