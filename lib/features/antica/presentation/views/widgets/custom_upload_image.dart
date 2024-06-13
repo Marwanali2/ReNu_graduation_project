62c54fe866bb0d89fcca2b9d99800cb45ae5032d
Anan Hamdy <nonahamdy205@gmail.com>
2024-06-14 01:24:09 +0300

    edit list of bidders

--- lib/features/antica/presentation/views/widgets/custom_upload_image.dart
+++ lib/features/antica/presentation/views/widgets/custom_upload_image.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:graduation_project/core/helpers/constants.dart';
 import 'package:graduation_project/core/theme/colors.dart';
 import 'package:image_picker/image_picker.dart';
--- lib/features/antica/presentation/views/widgets/listview_of_bidders_of_antika.dart
+++ lib/features/antica/presentation/views/widgets/listview_of_bidders_of_antika.dart
@@ -4,9 +4,43 @@ import 'package:graduation_project/core/helpers/constants.dart';
 import 'package:graduation_project/core/theme/colors.dart';
 import 'package:graduation_project/core/theme/styles.dart';
 
-class BiddersOfAntikaListView extends StatelessWidget {
+class BiddersOfAntikaListView extends StatefulWidget {
   const BiddersOfAntikaListView({super.key});
 
+  @override
+  State<BiddersOfAntikaListView> createState() => _BiddersOfAntikaListViewState();
+}
+
+class _BiddersOfAntikaListViewState extends State<BiddersOfAntikaListView> {
+List<Map<String,String>> bidders=[
+  {
+  'image':'assests/images/Ellipse199.png',
+  'name':'Messi',
+  'time':'1h 34m ago',
+  'price':'220 EGP'
+  },
+  {
+  'image':'assests/images/Ellipse19.png',
+  'name':'Asmaa',
+  'time':'2h 34m ago',
+  'price':'225 EGP'
+
+  },
+  {
+  'image':'assests/images/Ellipse199.png',
+  'name':'ronaldo',
+  'time':'1h 30m ago',
+  'price':'230 EGP'
+
+  },
+  {
+  'image':'assests/images/Ellipse199.png',
+  'name':'mapabi',
+  'time':'1h 20m ago',
+  'price':'235 EGP'
+
+  },
+];
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -14,25 +48,25 @@ class BiddersOfAntikaListView extends StatelessWidget {
         padding: EdgeInsets.zero,
             scrollDirection: Axis.vertical,
             shrinkWrap: true,
-            itemCount:4 ,
+            itemCount:bidders.length ,
             itemBuilder: (context,index){
               return SizedBox(
                 height: 59,
                 child: Row(
                   children: [ 
-                    Image.asset('assests/images/Ellipse199.png'),
+                    Image.asset(bidders[index]['image']!),
                     Padding(
                       padding: const EdgeInsets.only(left: 10),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                         
-                          Text('Messi',style: Styles.textStyle13.copyWith(
+                          Text(bidders[index]['name']!,style: Styles.textStyle13.copyWith(
                             color:blackcolor,
                             fontWeight: FontWeight.w500,
                             fontFamily: poppinsBlackFont
                           ),),
-                         const Text('1h 34m ago',style: TextStyle(
+                          Text(bidders[index]['time']!,style:const TextStyle(
                             fontSize: 10,
                             color: Color(0xff616161
                 ),
@@ -45,7 +79,7 @@ class BiddersOfAntikaListView extends StatelessWidget {
                       
                     Padding(
                       padding: const EdgeInsets.only(left: 150),
-                      child: Text('EGP 220',style: Styles.textStyle13.copyWith(
+                      child: Text(bidders[index]['price']!,style: Styles.textStyle13.copyWith(
                         color: borderFormColor,
                         fontWeight: FontWeight.w500,
                         fontFamily: poppinsBlackFont
